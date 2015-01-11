44cf6e71bbe0de00bec083ed96cefdc0af26a80b
Markfred Chen <markfred.chen@gmail.com>
2015-01-12 00:27:26 +0800

    report generated in modal view

--- client/app/daily-update/daily.update.js
+++ client/app/daily-update/daily.update.js
@@ -9,15 +9,85 @@ angular.module('mcJiraToolsApp')
                 controller: 'DailyUpdateCtrl'
             });
     })
-    .controller('DailyUpdateCtrl', ['$scope', '$cookieStore', '$http', '$location', function ($scope, $cookieStore, $http, $location) {
+    .controller('DailyUpdateCtrl', ['$scope', '$cookieStore', '$http', '$location', '$modal', function ($scope, $cookieStore, $http, $location, $modal) {
         $scope.today = new Date().getTime();
         $scope.issues = {};
         $scope.taskTypes = ['Design Discussion', 'Coding', 'Code Review', 'Checklist', 'Test Case', 'Testing', 'Automation', 'Other'];
 
-        $scope.doneTasks = [];
-        $scope.todoTasks = [];
-        $scope.blockedTasks = [];
+        $scope.isDoneTask = function (task) {
+            return task.taskState == "Done";
+        };
+        $scope.toggleStory = function (story) {
+            if (!story.isHide) {
+                story.isHide = true;
+            } else {
+                story.isHide = !story.isHide;
+            }
 
+        };
+        $scope.initData = function () {
+            $scope.team = $cookieStore.get('team');
+            var postData = {
+                auth: $cookieStore.get('auth'),
+                rapidViewID: $cookieStore.get('rapidViewID')
+            }
+            $http.post('/api/jiras/issues', postData)
+                .success(function (data, status, headers, config) {
+
+                    $scope.issues = JSON.parse(data);
+                    angular.forEach($scope.issues.userStories, function (story) {
+                        angular.forEach(story.tasks, function (task) {
+                            if (task.summary.toLowerCase().indexOf('coding') != -1) {
+                                task.type = 'Coding';
+                            } else if (task.summary.toLowerCase().indexOf('code review') != -1) {
+                                task.type = 'Code Review';
+                            } else if (task.summary.toLowerCase().indexOf('design') != -1 || task.summary.toLowerCase().indexOf('discussion') != -1) {
+                                task.type = 'Design Discussion';
+                            } else if (task.summary.toLowerCase().indexOf('checklist') != -1) {
+                                task.type = 'Checklist';
+                            } else if (task.summary.toLowerCase().indexOf('test case') != -1) {
+                                task.type = 'Test Case';
+                            } else if (task.summary.toLowerCase().indexOf('testing') != -1) {
+                                task.type = 'Testing';
+                            } else if (task.summary.toLowerCase().indexOf('automation') != -1) {
+                                task.type = 'Automation';
+                            } else {
+                                task.type = 'Other';
+                            }
+                        });
+                    });
+                }).error(function (data, status, headers, config) {
+                    console.log(data.error);
+                    $location.path("/");
+                });
+
+        }
+        $scope.openReport = function () {
+
+            $modal.open({
+                templateUrl: 'report.html',
+                controller: 'ReportController',
+                size: 'lg',
+                resolve: {
+                    data: function () {
+                        return {
+                            "userStories": $scope.issues.userStories,
+                            "team": $scope.team,
+                            "today": $scope.today
+                        }
+                    }
+                }
+            });
+        }
+
+        $scope.initData();
+
+    }])
+    .controller('ReportController', function ($scope, $modalInstance, data) {
+        $scope.userStories = data.userStories;
+        $scope.team = data.team;
+        $scope.today = data.today;
+        console.log($scope.userStories);
         $scope.hasDoneTask = function (story) {
             var hasDoneTask = false;
             angular.forEach(story.tasks, function (task) {
@@ -26,8 +96,16 @@ angular.module('mcJiraToolsApp')
                 }
             });
             return hasDoneTask;
-        }
-
+        };
+        $scope.getDoneTasks = function (story) {
+            var doneTasks = [];
+            angular.forEach(story.tasks, function (task) {
+                if (task.isDone) {
+                    doneTasks.push(task);
+                }
+            });
+            return getIssueTypes(doneTasks).join();
+        };
         $scope.hasTodoTask = function (story) {
             var hasTodoTask = false;
             angular.forEach(story.tasks, function (task) {
@@ -37,21 +115,6 @@ angular.module('mcJiraToolsApp')
             });
             return hasTodoTask;
         };
-
-        $scope.isDoneTask = function(task) {
-            return task.taskState == "Done";
-        };
-
-        $scope.getDoneTasks = function (story) {
-            var doneTasks = [];
-            angular.forEach(story.tasks, function (task) {
-                if (task.isDone) {
-                    doneTasks.push(task);
-                }
-            });
-            return getIssueTypes(doneTasks).join();
-        }
-
         $scope.getTodoTasks = function (story) {
             var todoTasks = [];
             angular.forEach(story.tasks, function (task) {
@@ -61,19 +124,9 @@ angular.module('mcJiraToolsApp')
             });
             return getIssueTypes(todoTasks).join();
         };
-
-        $scope.toggleStory = function(story) {
-            if(!story.isHide) {
-                story.isHide = true;
-            }else{
-                story.isHide = !story.isHide;
-            }
-
-        };
-
         $scope.getAllTodoTasks = function () {
             var todoTasks = [];
-            angular.forEach($scope.issues.userStories, function (story) {
+            angular.forEach($scope.userStories, function (story) {
                 angular.forEach(story.tasks, function (task) {
                     if (task.isTodo) {
                         todoTasks.push(task);
@@ -82,7 +135,6 @@ angular.module('mcJiraToolsApp')
             });
             return todoTasks;
         }
-
         var getIssueTypes = function (tasks) {
             var types = [];
 
@@ -94,45 +146,4 @@ angular.module('mcJiraToolsApp')
             return types;
         }
 
-
-        $scope.initData = function () {
-            $scope.team = $cookieStore.get('team');
-            var postData = {
-                auth: $cookieStore.get('auth'),
-                rapidViewID: $cookieStore.get('rapidViewID')
-            }
-            $http.post('/api/jiras/issues', postData)
-                .success(function (data, status, headers, config) {
-
-                    $scope.issues = JSON.parse(data);
-                    angular.forEach($scope.issues.userStories, function(story) {
-                        angular.forEach(story.tasks, function (task) {
-                            if(task.summary.toLowerCase().indexOf('coding') != -1) {
-                                task.type = 'Coding';
-                            }else if(task.summary.toLowerCase().indexOf('code review') != -1) {
-                                task.type = 'Code Review';
-                            }else if(task.summary.toLowerCase().indexOf('design') != -1 || task.summary.toLowerCase().indexOf('discussion') != -1) {
-                                task.type = 'Design Discussion';
-                            }else if(task.summary.toLowerCase().indexOf('checklist') != -1) {
-                                task.type = 'Checklist';
-                            }else if(task.summary.toLowerCase().indexOf('test case') != -1) {
-                                task.type = 'Test Case';
-                            }else if(task.summary.toLowerCase().indexOf('testing') != -1) {
-                                task.type = 'Testing';
-                            }else if(task.summary.toLowerCase().indexOf('automation') != -1) {
-                                task.type = 'Automation';
-                            }else{
-                                task.type = 'Other';
-                            }
-                        });
-                    });
-                }).error(function (data, status, headers, config) {
-                    console.log(data.error);
-                    $location.path("/");
-                });
-
-        }
-
-        $scope.initData();
-
-    }]);
+    });
